6a5b16620106b30a4ffcb07cf000e871433ea6d1
Andres Felipe <andrespipe021028@gmail.com>
2020-08-09 02:55:25 -0500

    Añadiendo el config

---
+++ api/v1/app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+// constantes de la base de datos
+define("DBHOST", "localhost");
+define("DBNAME", "transpublic");
+define("DBUSER", "root");
+define("DBPASWORD", "monserrate1010");
+define("DBDRIVER", "mysql");
+define("DBCHARSET", "utf8");
+// datos del servidor
+define("PROTOCOL", "http");
+
+define("URL_APP", dirname(dirname(__FILE__)));
+define("URL_PROJECT", PROTOCOL . '://' . $_SERVER["HTTP_HOST"] . '/' . $_SERVER["REQUEST_URI"]);
+
+//datos de la empresa o cliente
